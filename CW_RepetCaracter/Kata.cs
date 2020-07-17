cb83dfbc55736eabf835b7d649f119f155652356
giselleLago <gisellelago25994@gmail.com>
2020-07-17 17:12:17 +0200

    Add project files.

---
+++ CW_RepetCaracter/Kata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CW_RepetCaracter
+{
+    public class Kata
+    {
+        public int DuplicateCount(string str)
+        {
+            var list = new List<string>();
+            for (int x = 0; x < str.Length; x++)
+            {
+                var countB = 0;
+                var str1 = str[x].ToString().ToUpper();
+                for (int y = 0; y < str.Length; y++)
+                {
+                    var str2 = str[y].ToString().ToUpper();
+                    if (string.Equals(str2, str1))
+                    {
+                        countB++;
+                    }
+                }
+
+                if (countB >= 2 && !list.Contains(str1))
+                {
+                    list.Add(str1);
+                }
+            }
+            return list.Count;
+        }
+    }
+}
+
---
+++ CW_RepetCaracter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_RepetCaracter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            Console.WriteLine(a.DuplicateCount("ABBA"));
+            Console.ReadKey();
+        }
+    }
+}
